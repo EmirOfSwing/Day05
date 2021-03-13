7714c7f80d2c695557b6339d22bca99974594c89
EmirOfSwing <emir.arh@gmail.com>
2021-03-13 03:12:10 +0100

    Added some more exercises

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+   var myNoun = "dog";
+   var myVerb = "ran";
+   var myAdjective = "big";
+   var myAdverb = "quickly";
+   var wordBlanks = "Her" + " " + "ugly" + " " + myNoun + " " + myVerb + " " + "straight" + " " + "at" + " " + "me" + " " + "with" + " " + "his" + " " + myAdjective + " " + "jaws" + " " + "open" + "," + " " + "so" + " " + "I" + " " + "ran" + " " + myAdverb + ".";
+    // Only change code above this line
+    return wordBlanks;
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
